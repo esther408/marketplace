2a036388ef9b493bab63b8f4bf2d8b273e2d34ab
esther408 <xxroes456@gmail.com>
2024-11-01 15:42:46 +0400

    tue

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,53 +27,45 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Login Basic </title>
+    <title>Register Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
 
     <meta name="description" content="" />
 
-   <link rel="stylesheet" href="../assets/css/style.css">
-    
-    
-
-    <!-- Icons. Uncomment required icon fonts -->
-    
+    <link rel="stylesheet" href="../assets/css/style.css">
   </head>
 
-  <body bgcolor="white">
+  <body>
     <!-- Content -->
 
     <div class="container-xxl">
-      <div class="authentication-wrapper">
+      <div class="authentication-wrapper authentication-basic container-p-y">
         <div class="authentication-inner">
-          <!-- Register -->
+          <!-- Register Card -->
           <div class="card">
-       
             <div class="card-body">
-              <img src="public/assets/img/ape.png" alt="">
-              <h1>LOGIN HERE</h1>
-              <h4 class="mb-2">Welcome to market place! 👋</h4>
-              <p class="mb-4">Please sign-in to your account and start the adventure</p>
+              <!-- Logo -->
+             
+              <!-- /Logo -->
+              <h4 class="mb-2">CREATE AN ACCOUNT HERE🚀</h4>
 
-              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+              <form id="formAuthentication" class="mb-3" action="" method="POST">
                 <div class="mb-3">
-                  <label for="email" class="form-label">Email or Username</label><br>
+                  <label for="username" class="form-label">Username</label><BR>
                   <input
                     type="text"
                     class="form-control"
-                    id="email"
-                    name="email-username"
-                    placeholder="Enter your email or username"
+                    id="username"
+                    name="username"
+                    placeholder="Enter your username"
                     autofocus
                   />
-                </div>
+                </div><BR>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email</label><BR>
+                  <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" />
+                </div><BR>
                 <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label><br>
-                    <a href="/for">
-                      <small>Forgot Password?</small><br>
-                    </a>
-                    <a href="/home">dashboard</a>
-                  </div>
+                  <label class="form-label" for="password">Password</label><BR>
                   <div class="input-group input-group-merge">
                     <input
                       type="password"
@@ -82,51 +74,66 @@
                       name="password"
                       placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
                       aria-describedby="password"
-                    /><br>
+                    /><BR>
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                   </div>
                 </div>
+
                 <div class="mb-3">
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="remember-me" />
-                    <label class="form-check-label" for="remember-me"> Remember Me </label><br>
+                    <input class="form-check-input" type="checkbox" id="terms-conditions" name="terms" />
+                    <label class="form-check-label" for="terms-conditions">
+                      I agree to
+                      <a href="javascript:void(0);">privacy policy & terms</a>
+                    </label><br>
                   </div>
-                </div>
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                </div>
+                </div><br><a href="/">
+                <button>Sign up</button></a>
               </form>
 
               <p class="text-center">
-                <span>New on our platform?</span><br>
-                <a href="/register">
-                  <span>Create an account</span>
+                <span>Already have an account?</span><br>
+                <a href="/">
+                  <span>Sign in instead</span>
                 </a>
               </p>
             </div>
           </div>
-          <!-- /Register -->
+          <!-- Register Card -->
         </div>
       </div>
     </div>
 
     <!-- / Content -->
 
-   
+    <div class="buy-now">
+      <a
+        href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+        target="_blank"
+        class="btn btn-danger btn-buy-now"
+        >Upgrade to Pro</a
+      >
+    </div>
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
-    
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
 
     <!-- Main JS -->
-    
+    <script src="../assets/js/main.js"></script>
 
     <!-- Page JS -->
 
     <!-- Place this tag in your head or just before your close body tag. -->
-    
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
   </body>
 </html>
+
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.signin');
 });
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/for', function () {
+    return view('auth.for');
+});
+
+Route::get('/home', function () {
+    return view('dashboard.home');
+});
